422591d098de80b2b7a91d5fbcdb4ebd76d95293
Oliver <13oliver20@github.com>
2026-01-04 20:05:54 -0500

    Add debug to start.sh to check PORT variable

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# Debug: mostrar el valor de PORT
+echo "DEBUG: PORT variable is: ${PORT}"
+echo "DEBUG: Starting PHP server on port ${PORT}"
+
+# Iniciar servidor PHP
 php -S 0.0.0.0:${PORT} -t . router.php
